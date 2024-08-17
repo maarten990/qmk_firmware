b4b163d1380509d34be7a236f22df22421a4eedc
Maarten de Jonge <mdejonge1990@gmail.com>
2024-08-17 23:27:21 +0200

    add swapper function

--- keyboards/splitkb/aurora/sweep/keymaps/mine/keymap.c
+++ keyboards/splitkb/aurora/sweep/keymaps/mine/keymap.c
@@ -1,10 +1,14 @@
 #include QMK_KEYBOARD_H
 
 enum layers {
-    DEFAULT,
-    SYM,
-    NAV,
-    NUM,
+    DEFAULT = 0,
+    SYM = 1,
+    NAV = 2,
+    NUM = 3,
+};
+
+enum keycodes {
+    SW_WIN
 };
 
 #define OS_SHFT OSM(MOD_LSFT)
@@ -13,8 +17,9 @@ enum layers {
 #define OS_SUPR OSM(MOD_LGUI)
 #define TAB_N C(KC_TAB)
 #define TAB_P C(S(KC_TAB))
-#define WIND_N A(KC_TAB)
-#define WIND_P A(S(KC_TAB))
+
+
+bool sw_win_active = false;
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [DEFAULT] = LAYOUT(
@@ -30,8 +35,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                    _______, _______,     _______, _______
     ),
     [NAV] = LAYOUT(
-        _______, _______, KC_TAB,  KC_ESC,  A(KC_F4),    TAB_P,   WIND_P,  WIND_N,  TAB_N,   KC_DEL,
-        OS_SUPR, OS_ALT,  OS_CTRL, OS_SHFT, _______,     KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_BSPC,
+        _______, _______, KC_TAB,  KC_ESC,  _______,     TAB_P,   _______,  _______, TAB_N,  KC_DEL,
+        OS_SUPR, OS_ALT,  OS_CTRL, OS_SHFT, SW_WIN,      KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_BSPC,
         _______, _______, _______, _______, _______,     KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_ENT,
                                    _______, _______,     _______, _______
     ),
@@ -45,4 +50,38 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 layer_state_t layer_state_set_user(layer_state_t state) {
     return update_tri_layer_state(state, SYM, NAV, NUM);
+}
+
+void update_swapper(
+    bool *active,
+    uint16_t cmdish,
+    uint16_t tabish,
+    uint16_t trigger,
+    uint16_t keycode,
+    keyrecord_t *record
+) {
+    if (keycode == trigger) {
+        if (record->event.pressed) {
+            if (!*active) {
+                *active = true;
+                register_code(cmdish);
+            }
+            register_code(tabish);
+        } else {
+            unregister_code(tabish);
+            // Don't unregister cmdish until some other key is hit or released.
+        }
+    } else if (*active) {
+        unregister_code(cmdish);
+        *active = false;
+    }
+}
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    update_swapper(
+        &sw_win_active, KC_LALT, KC_TAB, SW_WIN,
+        keycode, record
+    );
+
+    return true;
 }
